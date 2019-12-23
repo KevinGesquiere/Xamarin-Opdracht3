9a78ec906fa6d0be7b3292fef718b3cdf33e1963
Kevin Gesquiere <kgesquiere@barclab.com>
2019-12-23 21:59:41 +0100

    Add models

---
+++ Opdracht3/Opdracht3/Domain/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht3.Domain.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime VisitDate { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
